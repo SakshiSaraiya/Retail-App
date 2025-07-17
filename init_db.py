338ef9ce97f03acd6f20935a54823aaeccf51a49
SAKSHI SARAIYA <sakshisaraiya@gmail.com>
2025-07-17 16:19:11 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,11 +1,12 @@
+
 import mysql.connector
 
 # Define your Clever Cloud DB credentials
 config = {
-    "host": "bv3ujjbvpicnpoubldj3-mysql.services.clever-cloud.com",
-    "user": "u1zhzkmobpodazeq",
-    "password": "umNG9W0xHh2MexvDoYqf",
-    "database": "bv3ujjbvpicnpoubldj3",
+    "host": "bi8sxvswtshvd8mok6ev-mysql.services.clever-cloud.com",
+    "user": "uxl6ku8uc7vfksfr",
+    "password": "KeB1IT85UYg0HcDY2UUm",
+    "database": "bi8sxvswtshvd8mok6ev",
     "port": 3306
 }
 
